96e007f02672eebd34cd169b0dfa27824f4d30e4
pandalinds <aleclee1996@gmail.com>
2020-02-25 20:05:48 -0700

    Coffe Implementation Completed

---
+++ UnityProject/Assets/Scripts/Espresso.cs
@@ -0,0 +1,12 @@
+﻿public class Espresso : ICoffee
+{
+    public string GetDescription()
+    {
+        return "Espresso made with care";
+    }
+ 
+    public double GetCost()
+    {
+        return 2.99;
+    }
+}
