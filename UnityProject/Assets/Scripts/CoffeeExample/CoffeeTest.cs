f427d591d886c1af26d7ea44dd2511a7cb0939a4
pandalinds <aleclee1996@gmail.com>
2020-02-26 23:18:07 -0700

    Finished M04DV01 for Coffee
    Instructions for gameplay will be displayed in GUI

--- UnityProject/Assets/Scripts/CoffeeExample/CoffeeTest.cs
+++ UnityProject/Assets/Scripts/CoffeeExample/CoffeeTest.cs
@@ -4,47 +4,67 @@ using UnityEngine;
 
 public class CoffeeTest : MonoBehaviour
 {
-    public void ShouldSupportCondiments()
-{
-    var beverages = new List<ICoffee>
-    {
-        new ChocolateDecorator(new Filtered()),
-        new ChocolateDecorator(new MilkDecorator(new Espresso()))
-    };
- 
-    var filteredWithChocolate = beverages[0]; 
-    if("Filtered with care, Chocolate" != filteredWithChocolate.GetDescription())
-    {
-        return;
-    }
-    if((1.99 + 0.29) != filteredWithChocolate.GetCost())
-    {
-        return;
-    }
-    Debug.Log("Filtered with coffee discription and cost: "+filteredWithChocolate.GetDescription()+" "+filteredWithChocolate.GetCost());
- 
-    var espressoWithMilkAndChocolate = beverages[1];
-    if("Espresso made with care, Milk, Chocolate" != espressoWithMilkAndChocolate.GetDescription())
-    {
-        return;
-    }
-    if((2.99 + 0.19 + 0.29) != espressoWithMilkAndChocolate.GetCost())
-    {
-        return;
-    }
-    Debug.Log("Milk and Chocolate coffee discription and cost: "+espressoWithMilkAndChocolate+" "+espressoWithMilkAndChocolate.GetCost());
-    Debug.Log("Espressos made!");
-}
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        ShouldSupportCondiments();
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            ICoffee coffee = new Espresso();
+            Debug.Log("Coffee discription and cost: " + coffee.GetDescription() + " $" + coffee.GetCost());
+        }
+
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            ICoffee coffee = new Filtered();
+            Debug.Log("Coffee discription and cost: " + coffee.GetDescription() + " $" + coffee.GetCost());
+        }
+
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            ICoffee coffee = new ChocolateDecorator(new Espresso());
+            Debug.Log("Coffee discription and cost: " + coffee.GetDescription() + " $" + coffee.GetCost());
+        }
+
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            ICoffee coffee = new ChocolateDecorator(new Filtered());
+            Debug.Log("Coffee discription and cost: " + coffee.GetDescription() + " $" + coffee.GetCost());
+        }
+
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            ICoffee coffee = new MilkDecorator(new Espresso());
+            Debug.Log("Coffee discription and cost: " + coffee.GetDescription() + " $" + coffee.GetCost());
+        }
+
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            ICoffee coffee = new MilkDecorator(new Filtered());
+            Debug.Log("Coffee discription and cost: " + coffee.GetDescription() + " $" + coffee.GetCost());
+        }
+
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            ICoffee coffee = new ChocolateDecorator(new MilkDecorator(new Filtered()));
+            Debug.Log("Coffee discription and cost: " + coffee.GetDescription() + " $" + coffee.GetCost());
+        }
+
+        if(Input.GetKeyDown(KeyCode.V))
+        {
+            ICoffee coffee = new ChocolateDecorator(new MilkDecorator(new Espresso()));
+            Debug.Log("Coffee discription and cost: " + coffee.GetDescription() + " $" + coffee.GetCost());
+        }
+    }
+
+    //game instructions
+    void OnGUI()
+    {
+        GUI.color = Color.black;
+        GUI.Label(new Rect(10, 10, 500, 20), "A - Espresso, S - Filtered");
+        GUI.Label(new Rect(10, 25, 500, 20), "D - Espresso w/ Chocolate, F - Filtered w/ Chocolate");
+        GUI.Label(new Rect(10, 40, 500, 20), "Z - Espresso w/ Milk, X - Filtered w/ Milk");
+        GUI.Label(new Rect(10, 55, 500, 20), "C - Espresso w/ Chocolate & Milk, V - Filtered w/ Chocolate & Milk");
+        GUI.Label(new Rect(10, 70, 500, 20), "Implemented coffee instead. Output is in Debug.Log!");
     }
 }
