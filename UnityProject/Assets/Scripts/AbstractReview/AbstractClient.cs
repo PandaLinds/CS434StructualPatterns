d42cfcf1f1be3e15f6971e5e8caaa8bb01233690
Alec Erno <aleclee1996@gmail.com>
2020-02-28 13:48:35 -0700

    Added review of factory
    Need review of abstract factory

---
+++ UnityProject/Assets/Scripts/AbstractReview/AbstractClient.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbstractClient : MonoBehaviour
+{
+    public NPCSpawner mNPCSpawner;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            mNPCSpawner.SpawnVillagers();
+        }
+        
+    }
+}
---
+++ UnityProject/Assets/Scripts/AbstractReview/NPCSpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class NPCSpawner : MonoBehaviour
+{
+    public NPCFactory mFactory;
+
+    private INPC mFarmer;
+    private INPC mBeggar;
+    private INPC mShopowner;
+
+    public void SpawnVillagers()
+    {
+        mFarmer = mFactory.GetNPC(NPCType.Farmer);
+        mBeggar = mFactory.GetNPC(NPCType.Beggar);
+        mShopowner = mFactory.GetNPC(NPCType.Shopowner);
+
+        mFarmer.Speak();
+        mBeggar.Speak();
+        mShopowner.Speak();
+    }
+}
